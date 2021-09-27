026ecd0ac23b552e3e22e02e3665f90901bceda3
MaxymSenkiv <maksym.senkiv.knm.2019@lpnu.ua>
2021-09-27 15:46:59 +0300

    Initial commit

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlaySnake2D()
+    {
+        SceneManager.LoadScene("Snake2D");
+    }
+    public void PlaySnake3D()
+    {
+        SceneManager.LoadScene("Snake3D");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
